f6b8b5611509526f937a1bbce32d77d363db48c8
Omar Othman <omar@Omars-MacBook-Pro.local>
2024-08-18 10:36:08 +0300

    init edf3

---
+++ android/app/src/main/kotlin/com/example/edf3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.edf3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainMenu());
+}
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(" TITLE "),
+        ),
+        body: const Center(
+          child: Text("Centered"),
+        ),
+      ),
+    );
+  }
+}
