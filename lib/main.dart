79a75df389513a7eb83933f3cd670105dfa87923
Eng Fo2ad <folathkani@gmailcom>
2024-08-19 11:07:20 +0300

    here is the card widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'features/cards/presentation/bloc/cards_bloc.dart';
 
 void main() {
   runApp(const MainMenu());
 }
 
 class MainMenu extends StatelessWidget {
+  // final String cardNumber = 'XXXX XXXX XXXX XXXX';
+  // final String expiryDate = 'MM/YY';
+  // final String cardHolderName = 'Card Holder';
+  // final String cvvCode = '123';
+  // final bool isCvvFocused = false;
+
   const MainMenu({super.key});
 
   @override
@@ -12,10 +20,10 @@ class MainMenu extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(" TITLE "),
+          title: const Text("Edfa3"),
         ),
-        body: const Center(
-          child: Text("Centered"),
+        body: Center(
+          child: CreditCard(),
         ),
       ),
     );
