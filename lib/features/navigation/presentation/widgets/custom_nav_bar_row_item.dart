6fd952508da4dec3124efc38dd7a3f848701cdfb
Eng Fo2ad <folathkani@gmailcom>
2024-09-13 11:38:56 +0300

    fixed button navigation bar 'qr' bug

--- lib/features/navigation/presentation/widgets/custom_nav_bar_row_item.dart
+++ lib/features/navigation/presentation/widgets/custom_nav_bar_row_item.dart
@@ -19,6 +19,48 @@ class CustomNavBarRowItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget myWidget = icon == AppIcons.qr
+        ? ButtonNavigationBarItem(
+            isHightLighted: isHightLighted,
+            index: index,
+            icon: icon,
+            text: text)
+        : ClipOval(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                splashColor: Colors.grey[300]?.withOpacity(0.7),
+                onTap: () {
+                  if (index != 2) onTap(index);
+                },
+                child: ButtonNavigationBarItem(
+                    isHightLighted: isHightLighted,
+                    index: index,
+                    icon: icon,
+                    text: text),
+              ),
+            ),
+          );
+    return myWidget;
+  }
+}
+
+class ButtonNavigationBarItem extends StatelessWidget {
+  const ButtonNavigationBarItem({
+    super.key,
+    required this.isHightLighted,
+    required this.index,
+    required this.icon,
+    required this.text,
+  });
+
+  final bool isHightLighted;
+  final int index;
+  final AppIcons icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
     final Color baseColor =
         isHightLighted ? AppTheme.hightLighted : AppTheme.notHightLighted;
     final Image image = Image.asset(
@@ -27,45 +69,34 @@ class CustomNavBarRowItem extends StatelessWidget {
       width: 24,
       color: baseColor,
     );
-    return ClipOval(
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          splashColor: Colors.grey[300]?.withOpacity(0.7),
-          onTap: () {
-            onTap(index);
-          },
-          child: SizedBox(
-            height: 75,
-            width: 70,
-            child: Column(
-              children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 17,
-                          child: Divider(
-                            thickness: 2,
-                            color: isHightLighted && index != 2
-                                ? baseColor
-                                : Colors.transparent,
-                            height: 20,
-                          ),
-                        )
-                      ],
+    return SizedBox(
+      height: 75,
+      width: 70,
+      child: Column(
+        children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 17,
+                    child: Divider(
+                      thickness: 2,
+                      color: isHightLighted && index != 2
+                          ? baseColor
+                          : Colors.transparent,
+                      height: 20,
                     ),
-                  ] +
-                  (icon != AppIcons.qr
-                      ? [
-                          image,
-                          Text(text),
-                        ]
-                      : []),
-            ),
-          ),
-        ),
+                  )
+                ],
+              ),
+            ] +
+            (icon != AppIcons.qr
+                ? [
+                    image,
+                    Text(text),
+                  ]
+                : []),
       ),
     );
   }
